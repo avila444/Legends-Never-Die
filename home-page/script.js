6e630f3ee03804750c051b43fa29d7021f711c08
nicolasmlongo@gmail.com <nicolasmlongo@gmail.com>
2022-11-18 17:11:00 -0300

    tentando fazer o carrossel

--- home-page/script.js
+++ home-page/script.js
@@ -1,1 +1,16 @@
-console.log("from script file");
+let slideIndex = 0;
+showSlides():
+
+function showSlides(){
+	let i;
+	let slides = document.getElementByClassName("slides");
+	for (i = 0; i < slides.lenght; i++){
+		slides[i].style.display = "none";
+	}
+	slideIndex++;
+	if (slideIndex > slideIndex){
+		slideIndex = 1;
+	}
+	slides[slideIndex-1].style.display = "block";
+	setTimeout(showSlides, 5000);
+}
