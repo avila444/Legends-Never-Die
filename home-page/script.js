47ff3f88cd96255dab7382b5287c7c93710d83f6
nicolasmlongo@gmail.com <nicolasmlongo@gmail.com>
2022-11-22 11:44:35 -0300

    carousel

--- home-page/script.js
+++ home-page/script.js
@@ -1,16 +1,14 @@
-let slideIndex = 0;
-showSlides():
+var myIndex = 0;
+carousel();
 
-function showSlides(){
-	let i;
-	let slides = document.getElementByClassName("slides");
-	for (i = 0; i < slides.lenght; i++){
-		slides[i].style.display = "none";
-	}
-	slideIndex++;
-	if (slideIndex > slideIndex){
-		slideIndex = 1;
-	}
-	slides[slideIndex-1].style.display = "block";
-	setTimeout(showSlides, 5000);
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 2000);
 }
